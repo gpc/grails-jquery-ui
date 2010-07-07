eb5218398c0ccae1a62a267211e2ba1c389a8f72
marcpalmer <marcpalmer@832c1c66-4827-0410-8465-ccb17913cd09>
2010-07-07 16:04:31 +0000

    Updated to use jq plugin 1.4.2.5 which doesn't have printlns in

--- JqueryUiGrailsPlugin.groovy
+++ JqueryUiGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class JqueryUiGrailsPlugin {
     // the plugin version
-    def version = "1.8.2.3"
+    def version = "1.8.2.4"
 
     // @todo This MUST be updated when you update the version of jquery ui
     static JQUERYUI_VERSION = "1.8.2"
@@ -8,7 +8,7 @@ class JqueryUiGrailsPlugin {
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2 > *"
     // the other plugins this plugin depends on
-    def dependsOn = [jquery:'1.4.2.4 > *']
+    def dependsOn = [jquery:'1.4.2.5 > *']
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp"
