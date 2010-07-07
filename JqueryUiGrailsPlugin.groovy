d18b8bf0e4e88ebc1e1324f79da172ae532e8e0f
marcpalmer <marcpalmer@832c1c66-4827-0410-8465-ccb17913cd09>
2010-07-07 14:23:17 +0000

    More jQueryConfig fixes

--- JqueryUiGrailsPlugin.groovy
+++ JqueryUiGrailsPlugin.groovy
@@ -1,10 +1,9 @@
 class JqueryUiGrailsPlugin {
     // the plugin version
-    def version = "1.8.2.2"
+    def version = "1.8.2.3"
 
     // @todo This MUST be updated when you update the version of jquery ui
     static JQUERYUI_VERSION = "1.8.2"
-
 
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2 > *"
