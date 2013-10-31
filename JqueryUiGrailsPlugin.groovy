7b9befab0c50b3c13cfc029e7435e44b02a68385
Burt Beckwith <burt@burtbeckwith.com>
2013-10-31 11:22:54 -0400

    version must be a constant in 2.0+

--- JqueryUiGrailsPlugin.groovy
+++ JqueryUiGrailsPlugin.groovy
@@ -2,13 +2,13 @@ class JqueryUiGrailsPlugin {
     // @todo This MUST be updated when you update the version of jquery ui
     static JQUERYUI_VERSION = "1.8.16"
 
-    // Put in here the minor revision of this plugin, appended to the JQUERY UI version automatically to
-    // make plugin release version
+    // Put in here the minor revision of this plugin
     static PLUGIN_MINOR_REVISION = ""
 
-    // the plugin version
-    // NOTE: this does not compile correctly under Grails 2.0, use 1.3.7
-    def version = JQUERYUI_VERSION + (PLUGIN_MINOR_REVISION ? '.' + PLUGIN_MINOR_REVISION : '')
+    // the version attribute must be constant, so manually update this to be
+    // JQUERYUI_VERSION and PLUGIN_MINOR_REVISION concatenated, with a '.'
+    // if PLUGIN_MINOR_REVISION isn't blank
+    def version = '1.8.16'
 
 
     def grailsVersion = "1.3 > *"
