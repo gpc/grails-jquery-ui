d63fe4030f09cad806908a25747107f8d2b7da18
Burt Beckwith <burt@burtbeckwith.com>
2013-10-31 11:05:59 -0400

    update to Grails 2.x for development

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,16 +1,22 @@
-grails.project.class.dir = 'target/classes'
-grails.project.test.class.dir = 'target/test-classes'
-grails.project.test.reports.dir	= 'target/test-reports'
+grails.project.work.dir = 'target'
 
 grails.project.dependency.resolution = {
-	inherits 'global'
 
+	inherits 'global'
 	log 'warn'
 
 	repositories {
-		grailsPlugins()
-		grailsHome()
+		grailsCentral()
+		mavenLocal()
+		mavenCentral()
 	}
 
-	dependencies {}
+	dependencies {
+	}
+
+	plugins {
+		build ':release:2.2.1', ':rest-client-builder:1.0.3', {
+			export = false
+		}
+	}
 }
